47b9807f026f49f98ef9e34b7c5e743e96248806
Gustavo Camargo <camargo25.gustavo@gmail.com>
2021-03-10 22:44:00 -0300

    First commit

---
+++ character.rb
@@ -0,0 +1,11 @@
+class Character
+  def initialize(name, age, life, attack, defense, intelligence, force)
+    @name = name
+    @age = age
+    @life = life
+    @attack = attack
+    @defense = defense
+    @intelligence = intelligence
+    @force = force
+  end
+end
